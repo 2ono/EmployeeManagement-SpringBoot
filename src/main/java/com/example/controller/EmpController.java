61154a2234054325dbfaa3182c182e830fa65bd0
2ono <clt3555@gmail.com>
2023-05-24 16:06:29 +0900

    set GetMapping

--- src/main/java/com/example/controller/EmpController.java
+++ src/main/java/com/example/controller/EmpController.java
@@ -1,8 +1,19 @@
 package com.example.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.entity.Employee;
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+
+
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @Controller
 public class EmpController {
 	@GetMapping("/")
@@ -10,4 +21,18 @@ public class EmpController {
 	{
 		return "index";
 	}
+	
+	@GetMapping("/addemp")
+	public String addEmpForm()
+	{
+		return "addemp";
+	}
+	
+	@PostMapping("/register")
+	public String empRegister (@ModelAttribute Employee e) {
+		System.out.println(e);
+		return "addemp";
+	}
+	
+	
 }
--- src/main/java/com/example/demo/EmpSystemApplication.java
+++ src/main/java/com/example/demo/EmpSystemApplication.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.controller.EmpController;
+
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = EmpController.class)
 public class EmpSystemApplication {
 
 	public static void main(String[] args) {
